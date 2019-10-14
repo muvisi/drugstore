2904226784e91afa6841286326fdd19e6d507546
davismutinda <davismutinda4@gmail.com>
2019-10-14 16:11:25 +0300

    changes

--- src/app/views/patient-checkin/triage/triage.component.ts
+++ src/app/views/patient-checkin/triage/triage.component.ts
@@ -3,23 +3,27 @@ import { NgxNavigationWithDataComponent } from 'ngx-navigation-with-data';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ServiceService } from '../../../service.service';
 import { ToastrService } from 'ngx-toastr';
+import { DatePipe } from '@angular/common';
 @Component({
   selector: 'app-triage',
   templateUrl: './triage.component.html',
-  styleUrls: ['./triage.component.scss']
+  styleUrls: ['./triage.component.scss'],
+  providers:[DatePipe]
 })
 export class TriageComponent implements OnInit {
   triageForm: FormGroup;
   submitted = false;
+  status=false;
   allergies = [];
   selectedAllergy = [];
   patient: any = {};
-  notes = '';
   bmi = 0;
   pressure = '';
   priority = '';
+  observations:any = {};
+  observations_points =[];
   constructor(public navCtrl: NgxNavigationWithDataComponent, private formBuilder: FormBuilder, public service: ServiceService,
-    public toastr: ToastrService) {
+    public toastr: ToastrService, public datePipe: DatePipe) {
    this.patient = this.navCtrl.get('data');
   
   }
@@ -85,6 +89,15 @@ if ( this.selectedAllergy.indexOf(item) < 0) {
 
 console.log(this.selectedAllergy);
 }
+addObservation(item){
+  const index =this.observations_points.indexOf(item);
+  if(index< 0){
+    this.observations_points.push(item);
+  }else{
+    this.observations_points.splice(index,1)
+  }
+  console.log(this.observations_points);
+}
 onSubmit() {
   this.submitted = true;
   if (this.triageForm.invalid) {
@@ -93,11 +106,16 @@ onSubmit() {
 const data = this.triageForm.value;
 data.bmi = this.bmi;
 data.blood_pressure = this.pressure;
-data.notes = this.notes;
+data.priority = this.priority;
+this.observations.appointment_date = this.datePipe.transform(this.observations.appointment_date,'yyyy-MM-dd');
+this.observations.symptoms_onset = this.datePipe.transform(this.observations.symptoms_onset,'yyyy-MM-dd')
 console.log(data);
 const info = {
   triage : data,
+  observations:this.observations,
+  observations_points: this.observations_points,
   allergies: this.selectedAllergy,
+  status:this.status,
   visit_number: this.patient.visit_no
 };
 this.service.triageDetail(info).subscribe((res) => {
