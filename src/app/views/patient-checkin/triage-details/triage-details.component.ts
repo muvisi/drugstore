a145be0d6ab1afcf02974953e7e4036e4b616d97
davismutinda <davismutinda4@gmail.com>
2019-10-14 13:33:32 +0300

    changes

--- src/app/views/patient-checkin/triage-details/triage-details.component.ts
+++ src/app/views/patient-checkin/triage-details/triage-details.component.ts
@@ -6,6 +6,8 @@ import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { NgxNavigationWithDataComponent } from 'ngx-navigation-with-data';
 import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import 'rxjs/add/operator/filter';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
 @Component({
   selector: 'app-triage-details',
   templateUrl: './triage-details.component.html',
@@ -13,6 +15,7 @@ import 'rxjs/add/operator/filter';
 })
 export class TriageDetailsComponent implements OnInit {
   @ViewChild('staticModal', {'static': true}) staticModal: ModalDirective;
+  @ViewChild(MatPaginator, { static: true}) paginator: MatPaginator;
   form: FormGroup;
   @ViewChild('lower', {'static': true}) lower: ElementRef;
   @ViewChild('upper', {'static': true}) upper: ElementRef;
@@ -30,80 +33,27 @@ export class TriageDetailsComponent implements OnInit {
   patientName;
   triageSuccess = false;
   visit_no;
+  Columns: string[] = ['sn','name','patient_no','visit_no','national_id','priority','phone','status','date','time']
+  dataSource: any;
   constructor(public service: ServiceService, private toastr: ToastrService, private fb: FormBuilder,
   public navCtrl: NgxNavigationWithDataComponent) { }
 
   ngOnInit() {
-
-    const lowerLimit = 70;
-    const upperLimit = 150;
-    const weightLimit = 34;
-    const heightLimit = 30;
-    const temperatureLimit = 25;
-    const pulseLimit = 90;
-
-    this.form = this.fb.group({
-      lower: ['', [Validators.required, Validators.maxLength(lowerLimit)]],
-      upper: ['', [Validators.required, Validators.maxLength(upperLimit)]],
-      weight: ['', [Validators.required, Validators.maxLength(weightLimit)]],
-      height: ['', [Validators.required, Validators.maxLength(heightLimit)]],
-      temperature: ['', [Validators.required, Validators.maxLength(heightLimit)]],
-      pulse: ['', [Validators.required, Validators.maxLength(heightLimit)]],
-      respiratory: ['', [Validators.required, Validators.maxLength(heightLimit)]],
-      // height: ['', [Validators.required, Validators.maxLength(heightLimit)]]
-    });
-    console.log(this.form.get('lower'));
-    if (this.form.get('lower').value > 50) {
-      console.log(this.lower.nativeElement.value = 50 / 60);
-    }
-    this.form.get('lower').valueChanges
-      .filter((value: number) => value >= lowerLimit && value < 100)
-      .subscribe(() => this.upper.nativeElement.focus());
-
-    this.form.get('upper').valueChanges
-      .filter((value: number) => value >= upperLimit)
-      .subscribe(() => this.weight.nativeElement.focus());
-    this.form.get('weight').valueChanges
-      .filter((value: number) => value >= weightLimit)
-      .subscribe(() => this.height.nativeElement.focus());
-      this.form.get('height').valueChanges
-        .filter((value: number) => value >= heightLimit)
-      .subscribe(() => this.temperature.nativeElement.focus());
-     this.form.get('temperature').valueChanges
-       .filter((value: number) => value >= temperatureLimit)
-      .subscribe(() => this.pulse.nativeElement.focus());
-    this.form.get('pulse').valueChanges
-      .filter((value: number) => value <= pulseLimit)
-      .subscribe(() => this.respiratory.nativeElement.focus());
     this.triagePatients();
   }
   addTriage(item) {
     console.log(item);
     this.navCtrl.navigate('/dashboard/patients/add-triage/', {data: item });
   }
-  triageDetails() {
-    const triage = {
-      'blood_pressure': this.lower.nativeElement.value + '/' + this.upper.nativeElement.value,
-      'weight': this.weight.nativeElement.value,
-      'pulse': this.pulse.nativeElement.value,
-      'height': this.height.nativeElement.value,
-      'temperature': this.temperature.nativeElement.value,
-      'respiratory': this.respiratory.nativeElement.value,
-    };
-    const data = {
-      'visit_number': this.visit_no,
-      'triage': triage
-    };
-    this.service.triageDetail(data).subscribe((res) => {
-      console.log('success');
-      this.toastr.success('Successfully saved Triage details');
-      this.ngOnInit();
-      this.staticModal.hide();
-    });
+  search(filterValue: string) {
+    this.dataSource.filter = filterValue.trim().toLowerCase();
   }
+
   triagePatients() {
     this.service.triageList().subscribe((res) => {
       this.visits = res.results;
+      this.dataSource = new MatTableDataSource <[]>(res.results);
+      this.dataSource.paginator = this.paginator;
     }
     );
   }
