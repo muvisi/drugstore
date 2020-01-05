2a3f981bf7aaca88ef798b6c97e743b290ddf532
davismutinda <davismutinda4@gmail.com>
2020-01-05 12:29:22 +0300

    changes

--- src/app/service.service.ts
+++ src/app/service.service.ts
@@ -2,8 +2,8 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders, HttpErrorResponse, HttpResponse } from '@angular/common/http';
 import { Observable, of } from 'rxjs';
 import { map, catchError, tap } from 'rxjs/operators';
-export const endpoint = 'http://localhost:8000/';
-// export const endpoint = 'http://134.209.199.123:4000/';
+// export const endpoint = 'http://localhost:8000/';
+export const endpoint = 'http://134.209.199.123:4000/';
 @Injectable({
   providedIn: 'root'
 })
