c9c3b8460e7bbd560915c09a460f39a8d542d00d
davismutinda <davismutinda4@gmail.com>
2020-10-21 21:01:26 +0300

    chnanges

--- src/app/_nav.ts
+++ src/app/_nav.ts
@@ -40,102 +40,122 @@ export const navItems: NavData[] = [
     name: 'Main'
   },
   {
-    name: 'Patient Actions',
-    url: '/dashboard/patients',
-    icon: 'icon-people',
-    children: [
-      {
-        name: 'Patient Dashboard',
-        url: '/dashboard/patients',
-        icon: 'cui-laptop'
-      },
-      {
-        name: 'Add Patient',
-        url: '/dashboard/patients/new-patient/',
-        icon: 'icon-user-follow'
-      },
-      {
-        name: 'Patients List',
-        url: '/dashboard/patients/records/',
-        icon: 'fa fa-users'
-      },
-      {
-        name: 'Billing',
-        url: '/dashboard/patients/billing',
-        icon: 'fa fa-database'
-      },          
-      {
-          name: 'Insurecheck',
-          url: '/dashboard/patients/insure-check/',
-          icon: 'icon-magnifier-add',
-      },
-      {
+          name: 'Appointment',
+          url: '/dashboard/patients/appointments',
+          icon: 'cui-calendar',
+ },
+  {
         name: 'Triage',
         url: '/dashboard/patients/triage/',
-        icon: 'icon-info'
-      },
-      {
+        icon: 'fa fa-stethoscope'
+  },
+  {
         name: 'Treatment',
         url: '/dashboard/patients/diagnosis&treatment/',
         icon: 'fa fa-heartbeat'
-      },
-      {
-        name: 'Record',
-        url: '/dashboard/records-list',
-        icon: 'fa fa-list',
-      },
-       {
-        name: 'Calendar',
-        url: '/dashboard/patients/appointments',
-        icon: 'cui-calendar',
-      },
-    ]
   },
-  {
-    name: 'Pharmacy',
-    url: '/dashboard/pharmacy',
-    icon: 'fa fa-medkit',
-  },
-  {
-    name: 'eClaims',
-    url: '/dashboard/eclaims-dashboard',
-    icon: 'icon-layers',
-    // children: [
-    //   {
-    //     name: 'claims',
-    //     url: '/dashboard/eclaims-dashboard/claims',
-    //     icon: 'cui-justify-right'
-    //   },
-    //   {
-    //     name: 'Batch-claims',
-    //     url: '/dashboard/eclaims-dashboard/batching',
-    //     icon: 'fa fa-gift'
-    //   },
-    //   {
-    //     name: 'Batch-list',
-    //     url: '/dashboard/eclaims-dashboard/batch-list',
-    //     icon: 'cui-list'
-    //   }
-    // ]
-  },
-  {
-    name: 'Pharmacy Stock',
-    url: '/dashboard/stock-level',
-    icon: 'icon-social-dropbox',
-  },
-  {
-    name: 'Payments',
-    url: '/dashboard/payments',
-    icon: 'icon-wallet',
-  },
+  // {
+  //       name: 'Add Patient',
+  //       url: '/dashboard/patients/new-patient/',
+  //       icon: 'icon-user-follow'
+  // },
+  // {
+  //   name: 'Patient Actions',
+  //   url: '/dashboard/patients',
+  //   icon: 'icon-people',
+  //   children: [
+  //     {
+  //       name: 'Patient Dashboard',
+  //       url: '/dashboard/patients',
+  //       icon: 'cui-laptop'
+  //     },
+  //     {
+  //       name: 'Add Patient',
+  //       url: '/dashboard/patients/new-patient/',
+  //       icon: 'icon-user-follow'
+  //     },
+  //     {
+  //       name: 'Patients List',
+  //       url: '/dashboard/patients/records/',
+  //       icon: 'fa fa-users'
+  //     },
+  //     {
+  //       name: 'Billing',
+  //       url: '/dashboard/patients/billing',
+  //       icon: 'fa fa-database'
+  //     },          
+  //     {
+  //         name: 'Insurecheck',
+  //         url: '/dashboard/patients/insure-check/',
+  //         icon: 'icon-magnifier-add',
+  //     },
+  //     {
+  //       name: 'Triage',
+  //       url: '/dashboard/patients/triage/',
+  //       icon: 'icon-info'
+  //     },
+  //     {
+  //       name: 'Treatment',
+  //       url: '/dashboard/patients/diagnosis&treatment/',
+  //       icon: 'fa fa-heartbeat'
+  //     },
+  //     {
+  //       name: 'Record',
+  //       url: '/dashboard/records-list',
+  //       icon: 'fa fa-list',
+  //     },
+  //      {
+  //       name: 'Calendar',
+  //       url: '/dashboard/patients/appointments',
+  //       icon: 'cui-calendar',
+  //     },
+  //   ]
+  // },
+  // {
+  //   name: 'Pharmacy',
+  //   url: '/dashboard/pharmacy',
+  //   icon: 'fa fa-medkit',
+  // },
+  // {
+  //   name: 'eClaims',
+  //   url: '/dashboard/eclaims-dashboard',
+  //   icon: 'icon-layers',
+  //   children: [
+  //     {
+  //       name: 'claims',
+  //       url: '/dashboard/eclaims-dashboard/claims',
+  //       icon: 'cui-justify-right'
+  //     },
+  //     {
+  //       name: 'Batch-claims',
+  //       url: '/dashboard/eclaims-dashboard/batching',
+  //       icon: 'fa fa-gift'
+  //     },
+  //     {
+  //       name: 'Batch-list',
+  //       url: '/dashboard/eclaims-dashboard/batch-list',
+  //       icon: 'cui-list'
+  //     }
+  //   ]
+  // },
+  // {
+  //   name: 'Pharmacy Stock',
+  //   url: '/dashboard/stock-level',
+  //   icon: 'icon-social-dropbox',
+  // },
+  // {
+  //   name: 'Payments',
+  //   url: '/dashboard/payments',
+  //   icon: 'icon-wallet',
+  // },
   {
     name: 'Reports',
     url: '/dashboard/reports',
     icon: 'icon-chart',
   },
-  {
-    name: 'Setup',
-    url: '/dashboard/set-up',
-    icon: 'icon-wrench'
-    },
+  // {
+  //   name: 'Setup',
+  //   url: '/dashboard/set-up',
+  //   icon: 'icon-wrench'
+  //   },
 ];
--- src/app/views/patient-checkin/appointment/appointment.component.ts
+++ src/app/views/patient-checkin/appointment/appointment.component.ts
@@ -16,24 +16,37 @@ export class AppointmentComponent implements OnInit {
   appointmentForm: FormGroup;
   limit = new Date();
   toadysList;
+  submitted=false;
   calendarPlugins = [dayGridPlugin]; 
-  displayedColumns: string[] = ['patient_no', 'name','phone','appointment_date','time'];
+  displayedColumns: string[] = ['patient_no', 'name','phone','appointment_date','time','reason'];
   listColumns: string[] = ['S/No', 'name','phone','time'];
-  timing =['8:00AM','8:20AM','8:40AM','9:00AM','8:00AM','8:20AM','8:40AM','9:00AM','9:20AM','9:40AM','10:00AM','10:20AM','10:40AM','11:00AM','11:20AM','11:40AM','12:00PM',
+  timing =['8:00AM','8:20AM','8:40AM','9:00AM','9:20AM','9:40AM','10:00AM','10:20AM','10:40AM','11:00AM','11:20AM','11:40AM','12:00PM',
   '12:20AM','12:40AM','2:00PM','2:20PM','2:40PM','3:00PM','3:20PM','3:40PM','4:00PM','4:20PM','4:40PM','5:00PM','5:20PM','5:40PM']
   dataSource;
   @ViewChild('paginator', {static: true}) paginator: MatPaginator;
   @ViewChild('paginator1', {static: true}) paginator1: MatPaginator;
+  maxDate;
   constructor(public service: ServiceService,private formBuilder: FormBuilder,public toastr: ToastrService ) { }
-
   ngOnInit() {
+    this.maxDate = new Date();
     this.appointmentForm = this.formBuilder.group({
       appointment_date: ['', Validators.required],
       phone: ['',Validators.required],
       time: ['', Validators.required],
       reason: ['', Validators.required],
-      patient_no: ['', Validators.required],
-      name: ['',Validators.required]
+      first_name: ['', Validators.required],
+      other_names: [''],
+      last_name: ['', Validators.required],
+      gender: ['Female', Validators.required],
+      email: ['',Validators.email],
+      dob: ['', Validators.required],
+      priority: ['3', Validators.required],
+      residence: [''],
+      national_id: ['',Validators.required],
+      passport_no: [''],
+      occupation: [''],
+      code:['+254',Validators.required],
+      visit_type:['',Validators.required],
     });
     this.patientsList();
     this.appointments();
@@ -57,11 +70,11 @@ getList(){
   this.dataSource.filter = filterValue.trim().toLowerCase();
 }
  onSubmit() {
- 
   if (this.appointmentForm.invalid) {
     this.toastr.error('Fill in All the Fields Marked with *');
       return;
   }
+
   this.service.createAppointment(this.appointmentForm.value).subscribe((res)=>{
   this.toastr.success("Successfully addeded appointment");
   this.appointmentForm.reset();
