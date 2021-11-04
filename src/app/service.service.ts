44830cfc5a451f6334e0862cdd091c9cac81d051
davis mutuku <davismutinda4@gmail.com>
2021-11-04 11:40:55 +0300

    changes

--- src/app/service.service.ts
+++ src/app/service.service.ts
@@ -4,7 +4,6 @@ import { Observable, of } from 'rxjs';
 import { map, catchError, tap } from 'rxjs/operators';
 // export const endpoint='http://localhost:8000/';
 export const endpoint='http://134.209.199.123:9000/';
-
 @Injectable({
   providedIn: 'root'
 })
--- src/app/views/patient-checkin/appointment-details/appointment-details.component.ts
+++ src/app/views/patient-checkin/appointment-details/appointment-details.component.ts
@@ -35,7 +35,6 @@ export class AppointmentDetailsComponent implements OnInit {
   diagnosis:any ={};
   diagnosisSource;
   @ViewChild('staticModal', { static: false }) staticModal: ModalDirective;
-  @ViewChild('serviceModal', { static: false }) serviceModal: ModalDirective;
   @ViewChild('paginator', { static: true}) paginator: MatPaginator;
   Columns: string[] = ['sn','date','trans_id','name','msisdn','trans_type','amount','status','use']
   cashColumns: string[] = ['sn','date','name','amount','trx'];
@@ -54,157 +53,17 @@ export class AppointmentDetailsComponent implements OnInit {
   text: string;
   supervisionForm: FormGroup;
   insurances =[];
-  issues_tests_change={
-      "issues":[],
-      "tests":[]
-    }
-  issues =[
-    {
-      name:"Marital Issues",
-      code :101,
-      checked:false
-    },
-    {
-      name:"Relationship Issue",
-      code :102,
-      checked:false
-    },
-    {
-      name:"Loss & Grief",
-      code :103,
-      checked:false
-    },
-    {
-      name:"Stress/Anxiety",
-      code :104,
-      checked:false
-    },
-    {
-      name:"Self- Awareness/Esteem",
-      code :105,
-      checked:false
-    },
-    {
-      name:"Work/Career",
-      code :106,
-      checked:false
-    },
-    {
-      name:"Adolescence/Teenage/Parenting",
-      code :107,
-      checked:false
-    },
-    {
-      name:"Alcoholism/Substance Abuse",
-      code :108,
-      checked:false
-    },
-    {
-      name:"CDM",
-      code :109,
-      checked:false
-    },
-    {
-      name:"Trauma",
-      code :110,
-      checked:false
-    },
-    {
-      name:"Family Issues",
-      code :111,
-      checked:false
-    },
-    {
-      name:"Mental Health",
-      code :112,
-      checked:false
-    },
-    {
-      name:"Anger Management",
-      code :113,
-      checked:false
-    },
-    {
-      name:"Financial Issues",
-      code :114,
-      checked:false
-    },
-    {
-      name:"Gambling addiction",
-      code :115,
-      checked:false
-    },
-    {
-      name:"Relationship-Divorced/married/single/cohabiting/",
-      code :116,
-      checked:false
-    }
-  ]
-  tests=[
-    {
-      name:"DSM/ICD",
-      code:301,
-      slider:false,
-      value:1
-
-    },
-    {
-      name:"Sexual satisfaction",
-      code:302,
-      slider:true,
-      value:1
-    },
-    {
-      name:"Intelligent",
-      code:303,
-      slider:true,
-      value:1
-
-    },
-    {
-      name:"Drug test",
-      code:304,
-      slider:true,
-      value:1
-    },
-    {
-      name:"Depression",
-      code:305,
-      slider:true,
-      value:1
-    },
-    {
-      name:"Personality Test",
-      code:306,
-      slider:false,
-      value:1
-    }
-
-  ]
-  counsellling_type=[
-    {
-      type:"First Dose",
-      code:201,
-      value:"First Dose"
-
-    },
-    {
-      type:"Second Dose",
-      code:202,
-      value:"Second Dose"
-
-    }
-
-  ]
-
   addspouse=false;
   spouse=false;
   spouse_submiited=false;
   session_platform;
-
   loading=false;
   loading_count=0;
   triageForm: FormGroup;
+  counsellling_type=[
+    {type:"First Dose"},
+    {type:"Second Dose"}
+  ]
   constructor(private route: ActivatedRoute,public service:ServiceService,private formBuilder: FormBuilder,public toastr:ToastrService,public router:Router) { }
   ngOnInit() {
     this.appointmentForm = this.formBuilder.group({
@@ -215,7 +74,7 @@ export class AppointmentDetailsComponent implements OnInit {
     });
 
     this.triageForm = this.formBuilder.group({
-      temperature: ['', Validators.required],
+      temperature: ['', [Validators.required,Validators.min(10)]],
       weight: [''],
       systolic: [''],
       diastolic: [''],
@@ -248,11 +107,13 @@ export class AppointmentDetailsComponent implements OnInit {
     this.router.navigate(['/dashboard/bill-client/',this.route.snapshot.params.id])
    }
    triageSave(){
+     if(this.triageForm.invalid){
+       return 
+     }
      let data = this.triageForm.value;
      data.id = this.route.snapshot.params.id
      this.loading=true;
      this.service.postTriage(data).subscribe((res)=>{
-       
        this.loading=false;
        this.toastr.success("Triage Saved")
        console.log(res);
@@ -279,33 +140,9 @@ export class AppointmentDetailsComponent implements OnInit {
       this.loading = false
       this.customer = this.data.patient;
       this.appointmentForm.patchValue({date:new Date(this.data.date),reason:this.data.reason,time:this.data.time,type:this.data.dose})
-      this.triageForm.patchValue(res.triage);
-      this.noteForm.patchValue({notes:res.notes});
-    //   if(this.data.type=='couple')this.addspouse=true; else this.addspouse=false;
-    //   this.session_platform=this.data.platform;
-    //   this.customer = this.data.client;
-    //   this.serviceSource = new MatTableDataSource(this.data.services);
-    //   this.diagnosisSource = new MatTableDataSource(this.data.diagnosis);
-    //   this.serviceSource.paginator = this.paginator;
-    //   this.getCash(res.client.id);
-    //   this.getPayments(this.customer.phone);
-    //   if(this.data.notes){
-    //     this.noteForm.patchValue({notes:this.data.notes})
-    //   }
-    //   if(this.data.counselor!= undefined && this.data.counselor.id != undefined){
-    //     this.onSelect(this.data.counselor.id);
-    //   }
-    //   if(this.data.supervisor!= undefined && this.data.supervisor.id!= undefined){
-    //     this.onCounsoler(this.data.supervisor.id);
-    //   }
-    //   if(this.data.Location){
-    //     let room = this.rooms.find(obj=>obj.name == this.data.Location);
-    //     this.counselorForm.patchValue({room:room.id});
-    //   }
-    //   console.log(moment(this.data.StartTime).format('H:mm'))
-    //   this.appointmentForm.patchValue({date:new Date(this.data.StartTime),reason:this.data.Description,time:moment(this.data.StartTime).format('H:mm'),type:this.data.type})
-    // this.loading_count=this.loading_count+1
-    // if(this.loading_count==7)this.loading=false;
+      if(res.triage !=undefined){
+        this.triageForm.patchValue(res.triage);  
+      }
     },(err)=>{
       this.loading = false
     })
@@ -333,19 +170,24 @@ export class AppointmentDetailsComponent implements OnInit {
   }
 
   completeAppointment(){
-    let data = this.noteForm.value
-    data.id = this.route.snapshot.params.id
-    this.loading=true;
-    this.service.completeAppointment(data).subscribe((res)=>{
-      this.loading=false;
-      this.toastr.success("Successfully finished ",'Success');
-      this.ngOnInit();
-    },(err)=>{
-      this.loading=false;
-      this.toastr.info(err.error.error,"Failed");
-    })
-
+    if(this.triageForm.get('temperature').value == ''){
+      this.toastr.info("Kindly take temperature",'Temperature');
+    }
+    this.staticModal.show();
   }
-
+finish(){
+  let data = this.noteForm.value
+  data.id = this.route.snapshot.params.id
+  this.loading=true;
+  this.staticModal.hide();
+  this.service.completeAppointment(data).subscribe((res)=>{
+    this.loading=false;
+    this.toastr.success("Successfully finished ",'Success');
+    this.ngOnInit();
+  },(err)=>{
+    this.loading=false;
+    this.toastr.info(err.error.error,"Failed");
+  })
+}
  
 }
