164dd200591ff97a44cb4b0807dcbf3ce6f5afe8
Davis Mutinda <davismutinda4@gmail.com>
2021-12-23 07:03:40 +0300

    commit changes

--- src/app/service.service.ts
+++ src/app/service.service.ts
@@ -2,10 +2,10 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders, HttpErrorResponse, HttpResponse } from '@angular/common/http';
 import { Observable, of } from 'rxjs';
 import { map, catchError, tap } from 'rxjs/operators';
-// export const endpoint='http://localhost:8888/';
+export const endpoint='http://localhost:8001/';
 // export const endpoint='http://134.209.199.123:8888/';
 // export const endpoint='http://197.248.31.237:8548/';
-export const endpoint='https://bookings.aarhospital.com/';
+// export const endpoint='https://bookings.aarhospital.com/';
 @Injectable({
   providedIn: 'root'
 })
