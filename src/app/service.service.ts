9166ca0739dafad871eb0d0684fdc51c2a2448f5
davis mutuku <davismutinda4@gmail.com>
2022-01-20 09:15:35 +0300

    changes

--- src/app/service.service.ts
+++ src/app/service.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders, HttpErrorResponse, HttpResponse } from '@angular/common/http';
 import { Observable, of } from 'rxjs';
 import { map, catchError, tap } from 'rxjs/operators';
-// export const endpoint='http://localhost:8888/';
+// export const endpoint='http://localhost:8/';
 // export const endpoint='http://192.168.12.15:8888/';
 // export const endpoint = 'https://booking.healthixsolutions.com/';
 // export const endpoint='http://134.209.199.123:8888/';
