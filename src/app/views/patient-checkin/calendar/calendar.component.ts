e7e4cc0eaced2763e0f1142a152da33ccd713d84
samuel <mwangangimuvisi@gmail.com>
2022-01-26 10:18:12 +0300

    changes

--- src/app/views/patient-checkin/calendar/calendar.component.ts
+++ src/app/views/patient-checkin/calendar/calendar.component.ts
@@ -15,6 +15,7 @@ import { DateTimePicker } from '@syncfusion/ej2-calendars';
 export class CalendarComponent implements OnInit {
 token: string = sessionStorage.getItem('Token');
 selectedDate: Date = new Date();
+
 setView: View ='MonthAgenda';
 scheduleHours: WorkHoursModel  = { highlight: true, start: '08:00', end: '05:00' };
 public workWeekDays: number[] = [6];
@@ -26,44 +27,31 @@ dataManager: DataManager = new DataManager({
   adaptor: new UrlAdaptor,
   headers: [{ 'Authorization': 'Bearer ' + this.token}]
 });
-// public eventSettings: EventSettingsModel = { dataSource: this.dataManager };
+dataManager1: DataManager = new DataManager({
+  url: endpoint+'api/calreport/',
+  crudUrl: endpoint+'api/calreport/',
+  adaptor: new UrlAdaptor,
+  headers: [{ 'Authorization': 'Bearer ' + this.token}]
+});
+public eventSettings: EventSettingsModel = { dataSource: this.dataManager };
 public scheduleViews: View[] = ['Week', 'WorkWeek', 'Month', 'TimelineWeek', 'TimelineWorkWeek'];
 public showWeekend: boolean = true;
-// public data: object[] = [{
-//   Id: 1,
-//   Subject: 'Meeting',
-//   StartTime: new Date(2018, 1, 15, 10, 0),
-//   EndTime: new Date(2018, 1, 15, 12, 30)
-//     }];
-// eventSettings: EventSettingsModel = { dataSource: this.data
-// }
-// selectedDate: Date = new Date(2018, 1, 15);
+
   constructor( public service: ServiceService) {
    }
 
+// public eventSettings: EventSettingsModel = { dataSource: this.dataManager };
+  //  eventSettings: EventSettingsModel = {
+  //   dataSource:this.dataManager
+    
+  //  }
+   
+
   ngOnInit() {
-    // this.getCounsellors()
+   
   }
 
 onPopupOpen(args: PopupOpenEventArgs): void {
-  // if (args.type === 'Editor') {
-  //     let statusElement: HTMLInputElement = args.element.querySelector('#EventType') as HTMLInputElement;
-  //     if (!statusElement.classList.contains('e-dropdownlist')) {
-  //         let dropDownListObject: DropDownList = new DropDownList({
-  //             placeholder: 'Choose status', value: statusElement.value,
-  //             dataSource: ['New', 'Requested', 'Confirmed']
-  //         });
-  //         dropDownListObject.appendTo(statusElement);
-  //         statusElement.setAttribute('name', 'EventType');
-  //     }
-  //     let startElement: HTMLInputElement = args.element.querySelector('#StartTime') as HTMLInputElement;
-  //     if (!startElement.classList.contains('e-datetimepicker')) {
-  //         new DateTimePicker({ value: new Date(startElement.value) || new Date() }, startElement);
-  //     }
-  //     let endElement: HTMLInputElement = args.element.querySelector('#EndTime') as HTMLInputElement;
-  //     if (!endElement.classList.contains('e-datetimepicker')) {
-  //         new DateTimePicker({ value: new Date(endElement.value) || new Date() }, endElement);
-  //     }
-  // }
+ 
 }
 }
