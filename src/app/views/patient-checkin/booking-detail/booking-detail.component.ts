56ed2af2fc6970e1254a6e731908293d6243fa5f
Davis Mutinda <davismutinda4@gmail.com>
2022-02-17 08:24:18 +0300

    commit changes

--- src/app/views/patient-checkin/booking-detail/booking-detail.component.ts
+++ src/app/views/patient-checkin/booking-detail/booking-detail.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
-// import { ToastrService } from 'ngx-toastr';
-// import { ServiceService } from '../../../service.service';
+import { ToastrService } from 'ngx-toastr';
+import { ServiceService } from '../../../service.service';
 
 @Component({
   selector: 'app-booking-detail',
@@ -22,7 +22,7 @@ export class BookingDetailComponent implements OnInit {
   customer;
   show_insurance;
   loading: boolean;
-  constructor(private formBuilder:FormBuilder,private route: ActivatedRoute) { }
+  constructor(private formBuilder:FormBuilder,private route: ActivatedRoute, private service:ServiceService,private toast:ToastrService) { }
 
   ngOnInit() {
     this.clientForm = this.formBuilder.group({
@@ -55,78 +55,78 @@ export class BookingDetailComponent implements OnInit {
       nhif_number:['']
   
     });
-    // this.service.getbookingDetails(this.route.snapshot.params.id).subscribe((res)=>{
-    //   this.customer=res.patient;
-    //   this.patient_info={    
-    //       'dob': res.patient.dob != null ? res.patient.dob : '' ,
-    //       'email': res.patient.email != null ? res.patient.email : '',
-    //       'first_name': res.patient.first_name != null ? res.patient.first_name : '',
-    //       'gender':  res.patient.gender != null ?  res.patient.gender : '',
-    //       'last_name':res.patient.last_name != null ? res.patient.last_name : '',
-    //       'national_id': res.patient.national_id != null ? res.patient.national_id : '' ,
-    //       'phone':res.patient.phone != null ? res.patient.phone : '',
-    //       'residence': res.patient.residence != null ? res.patient.residence : '',
-    //       'other_names': res.patient.other_names != null ? res.patient.other_names : '',
-    //       'occupation': res.patient.occupation !=null ? res.patient.occupation : ''
-    //   }
-    //   this.speciality=res.specialist.split(",")
-    //   this.symptoms=res.symptoms.split(",")
-    //   this.department=res.department;
-    //   this.payment_type={'payment':res.payment !=null ? res.payemnt : ''}
+    this.service.getbookingDetails(this.route.snapshot.params.id).subscribe((res)=>{
+      this.customer=res.patient;
+      this.patient_info={    
+          'dob': res.patient.dob != null ? res.patient.dob : '' ,
+          'email': res.patient.email != null ? res.patient.email : '',
+          'first_name': res.patient.first_name != null ? res.patient.first_name : '',
+          'gender':  res.patient.gender != null ?  res.patient.gender : '',
+          'last_name':res.patient.last_name != null ? res.patient.last_name : '',
+          'national_id': res.patient.national_id != null ? res.patient.national_id : '' ,
+          'phone':res.patient.phone != null ? res.patient.phone : '',
+          'residence': res.patient.residence != null ? res.patient.residence : '',
+          'other_names': res.patient.other_names != null ? res.patient.other_names : '',
+          'occupation': res.patient.occupation !=null ? res.patient.occupation : ''
+      }
+      this.speciality=res.specialist.split(",")
+      this.symptoms=res.symptoms.split(",")
+      this.department=res.department;
+      this.payment_type={'payment':res.payment !=null ? res.payemnt : ''}
       
 
-    //   console.log("pateint",this.patient_info);
-    //   if(res.payment=="Insurance"){
-    //     this.show_insurance=true;
-    //     this.service.getInsurance(res.id).subscribe((res2)=>{
-    //       console.log(res2);
+      console.log("pateint",this.patient_info);
+      if(res.payment=="Insurance"){
+        this.show_insurance=true;
+        this.service.getInsurance(res.id).subscribe((res2)=>{
+          console.log(res2);
 
-    //       this.payment_info={
-    //           "payment":this.payment_type.payment,
-    //           "insurance_company": res2.insurance_company !=  null ? res2.insurance_company : '',
-    //           "scheme_name":  res2.scheme_name !=  null ? res2.scheme_name : '',
-    //           "scheme_number":  res2.scheme_number !=  null ? res2.scheme_number : '',
-    //           "employee":  res2.employee !=  null ? res2.employee : '',
-    //           "employee_number":  res2.employee_number !=  null ? res2.employee_number : '',
-    //           "department": res2.department !=  null ? res2.department : '',
-    //           "member_number":  res2.member_number !=  null ? res2.member_number : '',
-    //           "member_name": res2.member_name !=  null ? res2.member_name : '',
-    //           "relation":  res2.relation !=  null ? res2.relation : '',
-    //           "card_number":  res2.card_number !=  null ? res2.card_number : '',
-    //           "id_number":  res2.id_number !=  null ? res2.id_number : '',
-    //           "nhif_number":  res2.nhif_number !=  null ? res2.nhif_number : ''
-    //         }
+          this.payment_info={
+              "payment":this.payment_type.payment,
+              "insurance_company": res2.insurance_company !=  null ? res2.insurance_company : '',
+              "scheme_name":  res2.scheme_name !=  null ? res2.scheme_name : '',
+              "scheme_number":  res2.scheme_number !=  null ? res2.scheme_number : '',
+              "employee":  res2.employee !=  null ? res2.employee : '',
+              "employee_number":  res2.employee_number !=  null ? res2.employee_number : '',
+              "department": res2.department !=  null ? res2.department : '',
+              "member_number":  res2.member_number !=  null ? res2.member_number : '',
+              "member_name": res2.member_name !=  null ? res2.member_name : '',
+              "relation":  res2.relation !=  null ? res2.relation : '',
+              "card_number":  res2.card_number !=  null ? res2.card_number : '',
+              "id_number":  res2.id_number !=  null ? res2.id_number : '',
+              "nhif_number":  res2.nhif_number !=  null ? res2.nhif_number : ''
+            }
 
-    //         console.log("payement",this.payment_info);
-    //     this.paymentForm.patchValue(this.payment_info);
+            console.log("payement",this.payment_info);
+        this.paymentForm.patchValue(this.payment_info);
 
-    //     })
+        })
         
 
 
-    //   }
+      }
       
-    //   this.clientForm.patchValue(this.patient_info);
-    //   this.paymentForm.patchValue(this.payment_type);
-    // })
+      this.clientForm.patchValue(this.patient_info);
+      this.paymentForm.patchValue(this.payment_type);
+    })
    
 
   }
   feedbackClicked(){
     this.loading=true;
-    // this.service.getFeedbackLink({"phone":this.customer.phone}).subscribe((res)=>{
-    //   // this.toast.success("Successful")
-    //   this.loading=false
-    // },(err)=>{
-    //   // this.toast.warning("Failed")
-    //   this.loading=false
-    // });
+    this.service.getFeedbackLink({"phone":this.customer.phone}).subscribe((res)=>{
+      this.toast.success("Successful")
+      this.loading=false
+    },(err)=>{
+      this.toast.warning("Failed")
+      this.loading=false
+    });
   }
-  // ngAfterViewInit(){
-  //   if(this.clientForm!=null)
-  //     this.clientForm.patchValue(this.patient_info);
-  //     this.paymentForm.patchValue(this.payment_type);
-  // }
+  ngAfterViewInit(){
+    if(this.clientForm!=null)
+      this.clientForm.patchValue(this.patient_info);
+      this.paymentForm.patchValue(this.payment_type);
+  }
   get c() { return this.clientForm.controls; }
   get p() { return this.paymentForm.controls; }
 }
