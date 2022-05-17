3a040178a253dfa19839daaed303dea8c8ee6b4b
antonymwangig <antonymwangig@gmail.com>
2022-05-17 10:09:34 +0300

    commit changes

--- src/app/service.service.ts
+++ src/app/service.service.ts
@@ -13,13 +13,13 @@ const httpOptions_payments = {
 };
 
 
-export const endpoint='http://localhost:8000/';
+// export const endpoint='http://localhost:8000/';
 // export const endpoint='http://134.209.199.123:8787/';
 // export const endpoint='https://booking.healthixsolutions.com/';
 // export const endpoint = 'https://booking.healthixsolutions.com/';
 // export const endpoint='http://134.209.199.123:8080/';
 // export const endpoint='http://192.168.12.15:7778/';
-// export const endpoint='https://bookings.aarhospital.com/';
+export const endpoint='https://bookings.aarhospital.com/';
 // export const endpoint='https://booking.healthixsolutions.com/';
 // export const endpoint = 'https://booking.healthixsolutions.com/';
 // export const endpoint='http://134.209.199.123:8888/';
