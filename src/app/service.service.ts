bece1bdd3452a11bc1addade807571bf31b28c0c
antonymwangig <antonymwangig@gmail.com>
2022-08-17 07:50:06 +0300

    assfdas

--- src/app/service.service.ts
+++ src/app/service.service.ts
@@ -10,10 +10,10 @@ import { map, catchError, tap } from 'rxjs/operators';
 // export const endpoint='http://localhost:8000/';
 // export const endpoint='http://localhost:8888/';
 // export const endpoint='http://134.209.199.123:8787/';
-export const endpoint = 'https://booking.healthixsolutions.com/';
+// export const endpoint = 'https://booking.healthixsolutions.com/';
 // export const endpoint='http://134.209.199.123:8080/';
 // export const endpoint='http://192.168.12.15:7778/';
-// export const endpoint='https://bookings.aarhospital.com/';
+export const endpoint='https://bookings.aarhospital.com/';
 // export const endpoint='http://134.209.199.123:8888/';
 // export const endpoint='http://197.248.31.237:8548/';
 export const SOCKET_URL="wss://booking.healthixsolutions.com/api/";
@@ -25,8 +25,8 @@ export const stk_url="https://payments.healthixsolutions.com/payments/pay/"
 export const HEALTHIX_BACKEND_URL_AAR ='https://aarclaims.healthixsolutions.com/';
 // export const HEALTHIX_BACKEND_URL_AAR ='http://localhost:8000/'
 
-// export const FRONT_END_URL="https://bookings.aarhospital.com/#/"
-export const FRONT_END_URL="http://134.209.199.123:8889/#/"
+export const FRONT_END_URL="https://bookings.aarhospital.com/#/"
+// export const FRONT_END_URL="http://134.209.199.123:8889/#/"
 
 @Injectable({
   providedIn: 'root'
